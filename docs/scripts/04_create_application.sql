55c09ba6e497e661eed2340d909efbe2cee08e90
Galileo <galileoga63@gmail.com>
2019-06-30 19:21:56 -0600

    Cinco campos para prueba.
    Tarea de 30/06/2019, Negocios Web

--- docs/scripts/04_create_application.sql
+++ docs/scripts/04_create_application.sql
@@ -6,4 +6,9 @@ CREATE TABLE `application` (
   `applicationcuenta` VARCHAR(45) NULL,
   `applicationauthor` VARCHAR(45) NULL,
   `applicationemail` VARCHAR(45) NULL,
+  `applicationReleaseDate` DATETIME NULL,
+  `applicationPhone` VARCHAR(45) NULL,
+  `applicationAddress` VARCHAR(45) NULL,
+  `applicationCompany` VARCHAR(45) NULL,
+  `applicationFrameWork` VARCHAR(45) NULL;
   PRIMARY KEY (`idapplication`));
--- views/ficha.view.tpl
+++ views/ficha.view.tpl
@@ -2,4 +2,9 @@
 <ul>
   <li><b>Cuenta: </b>{{applicationcuenta}}</li>
   <li><b>Email: </b>{{applicationemail}}</li>
+  <li><b>Fecha de Lanzamiento: </b>{{applicationReleaseDate}}</li>
+  <li><b>Teléfono: </b>{{applicationPhone}}</li>
+  <li><b>Dirección de Autor: </b>{{applicationAddress}}</li>
+  <li><b>Compañía: </b>{{applicationCompany}}</li>
+  <li><b>FrameWork: </b>{{applicationFrameWork}}</li>
 </ul>
